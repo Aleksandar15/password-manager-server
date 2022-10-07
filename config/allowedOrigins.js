457503bc33f6ccbf3a0de7275157f1a738660625
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-10-07 06:02:15 +0200

    Extra caution modified allowedOrigins config to accept ONLY my frontend

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,10 +1,3 @@
-const allowedOrigins = [
-  // 'https://www.yoursite.com',
-  // 'http://127.0.0.1:5500',
-  // 'http://localhost:3500',
-  "http://localhost:3000",
-  "http://localhost:3003",
-  "https://alek-password-manager.netlify.app",
-];
+const allowedOrigins = ["https://alek-password-manager.netlify.app"];
 
 module.exports = allowedOrigins;
