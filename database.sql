d111365c54a250d30f0f201c53224a39b09101b7
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2023-02-17 00:01:33 +0100

    Added more info

--- database.sql
+++ database.sql
@@ -40,7 +40,7 @@ UPDATE users SET refresh_token='{}' WHERE refresh_token=$1; -- Empty out the 're
 SELECT * FROM users LEFT JOIN passwords ON users.user_id = passwords.user_id ORDER BY password_id ASC;
 -- Get all* user's info of a particuler user by user_id:
 SELECT * FROM users LEFT JOIN passwords ON users.user_id = passwords.user_id WHERE users.user_id='fake-user-id-1' ORDER BY password_id ASC;
--- *All or particular data based on needs
+-- *Either all or particular data based on needs
 
 -- REMINDER COMMANDS:
 -- \l -- all databases
@@ -52,5 +52,7 @@ SELECT * FROM users LEFT JOIN passwords ON users.user_id = passwords.user_id WHE
 -- CREATE extension IF NOT EXISTS "uuid-ossp"; -- install uuid inside postgres database
 
 -- -- MIGHT ADD:
--- ON DELETE CASCADE -- is required to the FOREGIN KEY -> would allow to delete the referencing rows if I were to `DELETE FROM users WHERE user_id=$1`, otherwise will fail if there is data in `passwords` by the matching 'user_id'.
+-- ON DELETE CASCADE -- is required to the FOREGIN KEY -> would allow to delete the referencing rows ('passwords' table) if I were to `DELETE FROM users WHERE user_id=$1`, 
+-- otherwise will fail if there is any data in `passwords` table by the matching 'user_id'.
+-- Use-case scenario: if user wants to delete their account - their 'passwords vault' should also be removed.
 -- ALTER TABLE passwords DROP CONSTRAINT passwords_user_id_fkey, ADD CONSTRAINT passwords_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE;
